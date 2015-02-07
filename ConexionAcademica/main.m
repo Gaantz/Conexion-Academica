43c5794860f9e63246fbb6ba74fbc745209978f3
Gantz <cristian.palomino.rivera@hotmail.com>
2015-02-06 21:10:19 -0500

    Initial Commit

---
+++ ConexionAcademica/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConexionAcademica
+//
+//  Created by Cristian Palomino Rivera on 6/02/15.
+//  Copyright (c) 2015 Cristian Palomino Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
